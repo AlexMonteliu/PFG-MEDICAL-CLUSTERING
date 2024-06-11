76828677311f8480291d261bab4ab1ce7a8cd110
AlexMonteliu <monteliualex@gmail.com>
2024-06-12 00:21:33 +0200

    correcion borrado especialidades

--- myproject/aplicacion1/views.py
+++ myproject/aplicacion1/views.py
@@ -24,11 +24,11 @@ def hello(request):
         6: "6- Nefrología",
         7: "7- Neurología",
         8: "8- Cirugía General",
-        9: "9- Cardiovascular/Pulmonar",
+        9: "9- Procedimientos Cardiovasculares",
         10:"10 Cirugía de Hombro",
         11:"11- Manejo del Dolor",
-        12:"12- Cardiovascular/Pulmonar",
-        13:"13- Cardiovascular/Pulmonar",
+        12:"12- Estudios Pulmonares y Pruebas de Esfuerzo",
+        13:"13- Condiciones Valvulares y Ecocardiogramas",
         14:"14- Gastroenterología",
         15:"15- Radiología",
         16:"16- Ortopedia Extremidades Inferiores",
@@ -41,7 +41,7 @@ def hello(request):
         2: "Este cluster contiene documentos que son notas de progreso o registros SOAP (Subjective, Objective, Assessment, Plan). Los temas incluyen diabetes, hipertensión y otros aspectos dietéticos y de peso, que son comunes en las notas de seguimiento de pacientes.",
         3: "Este cluster agrupa documentos de consultas y exámenes de historia clínica y física. Los temas de pérdida de peso, bypass gástrico y dolor sugieren que se trata de consultas detalladas sobre el historial médico del paciente y evaluaciones físicas.",
         4: "Los documentos en este cluster están relacionados con informes de cirugía y transcripciones médicas. Las características indican un enfoque en la calidad y precisión de las transcripciones y reportes quirúrgicos.",
-        5: "Similar al Cluster 1, este cluster también se enfoca en ortopedia pero con un enfoque en condiciones y procedimientos de la columna cervical, como discectomías y fusiones.",
+        5: "Este cluster también se enfoca en ortopedia con un enfoque en condiciones y procedimientos de la columna cervical, como discectomías y fusiones.",
         6: "Los documentos en este cluster están relacionados con la nefrología, abordando condiciones renales, fallas, procedimientos como la colocación de stents y catéteres, y la hemodiálisis.",
         7: "Este cluster se centra en neurología, incluyendo procedimientos y condiciones neurológicas como craniotomías, hematomas subdurales y debilidades musculares. El uso de MRI y CT indica un enfoque en imágenes radiológicas neurológicas.",
         8: "Este cluster incluye documentos relacionados con diversas cirugías, desde urológicas y herdesde urológicas y hernias hasta biopsias, cirugías nasales, y procedimientos en gastroenterología y otorrinolaringología.",
@@ -49,17 +49,18 @@ def hello(request):
         10: "Este cluster trata sobre procedimientos quirúrgicos, específicamente relacionados con la eliminación de cuerpos extraños, reparaciones del manguito rotador, desbridamientos y cirugías de hombro.",
         11: "Los documentos en este cluster se centran en el manejo del dolor, incluyendo inyecciones epidurales, estudios de conducción nerviosa y manejo del dolor mediante diversas técnicas.",
         12: "Este cluster también se centra en el sistema cardiovascular y pulmonar, con documentos que mencionan pruebas de esfuerzo, broncoscopias y otros estudios pulmonares.",
-        13: "Similar a los clusters 9 y 12, este cluster abarca temas del sistema cardiovascular y pulmonar, con un enfoque en condiciones valvulares como fibrilación auricular, regurgitaciones y ecocardiogramas.",
+        13: "Este cluster abarca temas del sistema cardiovascular y pulmonar, con un enfoque en condiciones valvulares como fibrilación auricular, regurgitaciones y ecocardiogramas.",
         14: "Los documentos en este cluster están relacionados con la gastroenterología, abordando procedimientos como colonoscopias, laparoscopias, y cirugías de vesícula biliar y apendicectomías.",
         15: "Este cluster está enfocado en radiología, incluyendo estudios de CT en abdomen y pelvis, con y sin contraste, y otros estudios radiológicos.",
         16: "Este cluster trata sobre ortopedia, específicamente en extremidades inferiores, abarcando fracturas, fijaciones de articulaciones, y procedimientos en rodillas, pies y tobillos.",
         17: "Los documentos en este cluster están relacionados con obstetricia y ginecología, incluyendo temas de embarazo, procedimientos uterinos y vaginales, y cirugías ginecológicas."
     }
-
+    common_clusters_by_specialty = get_common_clusters_by_specialty(df)
     context = {
         'clusters_features': clusters_features,
         'cluster_titles': cluster_titles,
         'cluster_analysis': cluster_analysis,
+        'common_clusters_by_specialty': common_clusters_by_specialty
     }
 
     return render(request, 'hello.html', context)
